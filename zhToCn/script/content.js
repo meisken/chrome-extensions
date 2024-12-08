f24ac6d7bb09ebf6a8df80a3a0875b661775bfb2
meisken <meisken999@gmail.com>
2024-12-09 04:40:18 +0800

    new funciton: quick to zh

--- zhToCn/script/content.js
+++ zhToCn/script/content.js
@@ -1,22 +1,37 @@
-const mode = {
-    "zh-cn": { from: 'hk', to: 'cn' },
-    "cn-zh": { from: 'cn', to: 'hk' }
+const contextMenuIds = {
+    zhToCn: "zh-cn",
+    cnToZh: "cn-zh",
+    quickToZh: "quick-zh",
+}
+
+const openCCmode = {
+    [contextMenuIds.zhToCn]: { from: 'hk', to: 'cn' },
+    [contextMenuIds.cnToZh]: { from: 'cn', to: 'hk' },
+
 };
 
 chrome.runtime.onMessage.addListener( 
     function(request, sender, sendResponse) {
+        let result = "";
+        if(request.mode === contextMenuIds.zhToCn || request.mode === contextMenuIds.cnToZh){
+            const converter = OpenCC.Converter(openCCmode[request.mode]);
+            result = converter(request.selectedText);
 
-        if(request.mode === "zh-cn" || request.mode === "cn-zh"){
-            const converter = OpenCC.Converter(mode[request.mode]);
-            const result = converter(request.selectedText);
-            console.log(result)
        
             navigator.clipboard.writeText(result);
-            alert(`copied ${result} to your clipboard`)
+         
         }
 
+        if(request.mode === contextMenuIds.quickToZh || request.mode === contextMenuIds.ZhToQuick){
+            result = request.processedResult 
+       
+            navigator.clipboard.writeText(result);
+        }
+        //alert(`copied ${result} to your clipboard`)
   
         sendResponse()
             
     }
 );
+
+
